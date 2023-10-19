94f250be2932bebf35697192988e402ce25144ed
Steffan-S <steffanscholten@hotmail.com>
2023-10-19 12:41:17 +0200

    Created files & simple add,subtract,multiply and divide functions

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+	return num1 + num2;
+};
+
+function subtract(num1, num2) {
+	return num1 - num2;
+};
+
+function multiply(num1, num2) {
+	return num1 * num2;
+};
+
+function divide(num1, num2) {
+	return num1 / num2;
+};
+
+console.log(add(10, 10) + ' add');
+console.log(subtract(10, 5) + ' subtract');
+console.log(multiply(5, 5) + ' multiply');
+console.log(divide(8, 2) + ' divide');
