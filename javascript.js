d51cb3188cb355607419da839d2c3f3958d3fa39
Steffan-S <steffanscholten@hotmail.com>
2023-10-19 12:53:04 +0200

    Added variables & operate function
    Added variables to store firstnumber, secondnumber, operator and
    the result of calculation.
    Created a operate function to calculate the sum

--- javascript.js
+++ javascript.js
@@ -18,3 +18,28 @@ console.log(add(10, 10) + ' add');
 console.log(subtract(10, 5) + ' subtract');
 console.log(multiply(5, 5) + ' multiply');
 console.log(divide(8, 2) + ' divide');
+
+let firstNumber = 4;
+let secondNumber = 2;
+let operator = '-';
+let operatorResult;
+
+function operate(firstNum, operator, secondNum){
+    if (operator === '+'){
+        operatorResult = add(firstNum, secondNum);
+        console.log('Add function');
+    } else if (operator === '-'){
+        operatorResult = subtract(firstNum, secondNum);
+        console.log('Subtract function');
+    } else if (operator === '*'){
+        operatorResult = multiply(firstNum, secondNum);
+        console.log('Multiply function');
+    } else if (operator === '/'){
+        operatorResult = divide(firstNum, secondNum);
+        console.log('Divide function');
+    }
+    return operatorResult
+}
+
+operate(firstNumber, operator, secondNumber);
+console.log(operatorResult + ' operate result');
